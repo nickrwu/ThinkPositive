83dc567b7924da8226d34229b234d08f0af189b6
KeN <ao.daito@gmail.com>
2017-12-31 10:46:39 -0500

    Initial Commit

---
+++ EMDR/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EMDR
+//
+//  Created by Nick Wu on 12/31/17.
+//  Copyright © 2017 Nick Wu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
